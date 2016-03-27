0e93ef57b7cb5ac2e0c359e47abf7535517d6f13
Andi M Masanam <andimarimuthu@gmail.com>
2016-03-27 23:12:14 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,31 +1,42 @@
-# Exploratory Data Analysis - Assignment 2 - Q. #4
+## Question 4
 
-# Load ggplot2 library
-require(ggplot2)
+## Libraries needed:
+library(plyr)
+library(ggplot2)
 
-# Loading provided datasets - loading from local machine
+## Set the work directory
+setwd("C:\Users\bharathi_balaji\R")
+
+## read in the data
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-# Coal combustion related sources
-SCC.coal = SCC[grepl("coal", SCC$Short.Name, ignore.case=TRUE),]
+## subset our data for only coal-combustion
+coalcomb.scc <- subset(SCC, EI.Sector %in% c("Fuel Comb - Comm/Instutional - Coal", 
+  "Fuel Comb - Electric Generation - Coal", "Fuel Comb - Industrial Boilers, ICEs - 
+  Coal"))
 
-# Merge two data sets
-merge <- merge(x=NEI, y=SCC.coal, by='SCC')
-merge.sum <- aggregate(merge[, 'Emissions'], by=list(merge$year), sum)
-colnames(merge.sum) <- c('Year', 'Emissions')
+## comparisons so that we didn't ommit anything weird
+coalcomb.scc1 <- subset(SCC, grepl("Comb", Short.Name) & grepl("Coal", Short.Name))
 
-# Across the United States, how have emissions from coal combustion-related sources 
-# changed from 1999-2008?
+##Union
+coalcomb.codes <- union(coalcomb.scc$SCC, coalcomb.scc1$SCC)
 
-# Generate the graph in the same directory as the source code
-png(filename='plot4.png')
+##Subset
+coal.comb <- subset(NEI, SCC %in% coalcomb.codes)
 
-ggplot(data=merge.sum, aes(x=Year, y=Emissions/1000)) + 
-    geom_line(aes(group=1, col=Emissions)) + geom_point(aes(size=2, col=Emissions)) + 
-    ggtitle(expression('Total Emissions of PM'[2.5])) + 
-    ylab(expression(paste('PM', ''[2.5], ' in kilotons'))) + 
-    geom_text(aes(label=round(Emissions/1000,digits=2), size=2, hjust=1.5, vjust=1.5)) + 
-    theme(legend.position='none') + scale_colour_gradient(low='black', high='red')
+## get the PM25 values as well
+coalcomb.pm25year <- ddply(coal.comb, .(year, type), function(x) sum(x$Emissions))
+
+#rename the col
+colnames(coalcomb.pm25year)[3] <- "Emissions"
+
+##prepare to plot to png
+png("plot4.png")
+
+qplot(year, Emissions, data=coalcomb.pm25year, color=type, geom="line") + 
+stat_summary(fun.y = "sum", fun.ymin = "sum", fun.ymax = "sum", color = "purple", aes(shape="total"), geom="line") +
+geom_line(aes(size="total", shape = NA)) + ggtitle(expression("Coal Combustion" ~ PM[2.5] ~ "Emissions by Source Type and Year")) + 
+xlab("Year") + ylab(expression("Total" ~ PM[2.5] ~ "Emissions (tons)"))
 
 dev.off()
