81c09c29eac26c99c7e611f45014de6d6a7bf733
Andi M Masanam <andimarimuthu@gmail.com>
2016-03-27 22:47:35 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,26 +1,18 @@
-# Exploratory Data Analysis - Assignment 2 - Q. #1
+#Question 1
 
+#set the work directory where the data resides
+setwd("C:/Users/bharathi_balaji/R")
 
-# Loading provided datasets - loading from local machine
+#read the data files 
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-# Sampling
-NEI_sampling <- NEI[sample(nrow(NEI), size=2000, replace=F), ]
-
-# Aggregate
-Emissions <- aggregate(NEI[, 'Emissions'], by=list(NEI$year), FUN=sum)
-Emissions$PM <- round(Emissions[,2]/1000,2)
-
-# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
-# Using the base plotting system, make a plot showing the total PM2.5 emission from all sources 
-# for each of the years 1999, 2002, 2005, and 2008.
+tot.PM25yr <- tapply(NEI$Emissions, NEI$year, sum) 
 
 # Generate the graph in the same directory as the source code
 png(filename='plot1.png')
 
-barplot(Emissions$PM, names.arg=Emissions$Group.1, 
-        main=expression('Total Emission of PM'[2.5]),
-        xlab='Year', ylab=expression(paste('PM', ''[2.5], ' in Kilotons')))
-
+plot(names(tot.PM25yr), tot.PM25yr, type="l", xlab = "Year", ylab = expression
+     ("Total" ~ PM[2.5] ~"Emissions (tons)"), main = expression("Total US" ~ 
+     PM[2.5] ~ "Emissions by Year"), col="Blue")
 dev.off()
