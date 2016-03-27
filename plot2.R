78bc1d5d1f32d2a0285c0768694cb104d86e3216
Andi M Masanam <andimarimuthu@gmail.com>
2016-03-27 22:52:33 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,24 +1,16 @@
-# Exploratory Data Analysis - Assignment 2 - Q. #2
+#Question 2
+#Set the working directory
+setwd("C:\Users\bharathi_balaji\R")
 
-
-# Loading provided datasets - loading from local machine
+# Read the data file from the direcotory
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-# Sampling
-NEI_sampling <- NEI[sample(nrow(NEI), size=5000, replace=F), ]
+baltimore <- subset (NEI, fips == "24510")
+total.PM25yr <- tapply(baltimore$Emissions, baltimore$year, sum)
 
-# Subset data and append two years in one data frame
-MD <- subset(NEI, fips=='24510')
-
-# Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") 
-# from 1999 to 2008? Use the base plotting system to make a plot answering this question.
-
-# Generate the graph in the same directory as the source code
-png(filename='plot2.png')
-
-barplot(tapply(X=MD$Emissions, INDEX=MD$year, FUN=sum), 
-        main='Total Emission in Baltimore City, MD', 
-        xlab='Year', ylab=expression('PM'[2.5]))
-
-dev.off()
+#Plot
+png("plot2.png")
+plot(names(total.PM25yr), total.PM25yr, type = "l", xlab="Year", ylab= expression("Total" ~ PM[2.5] ~ "Emissions (tons)"), 
+main=expression("Total for Baltimore City" ~ PM[2.5] ~ "Emissions by Year"), col = "blue")
+dev.off()  
